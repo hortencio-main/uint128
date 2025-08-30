53011bb7af37375af9358e5680f9a766ba4707df
Hortencio <caua.hortencio@unesp.br>
2025-08-30 19:18:56 -0300

    Update uint128.go

--- uint128.go
+++ uint128.go
@@ -1,16 +1,16 @@
-package uin128
+package Uint128
 
 import (
     "math/bits"
     "fmt"
 )
 
-type uint128 struct {
+type Uint128 struct {
     H uint64
     L uint64
 }
 
-func (a uint128) Add128(b uint128) uint128 {
+func (a Uint128) Add128(b Uint128) Uint128 {
     l:= a.L + b.L
     
     carry := uint64(0)
@@ -19,13 +19,13 @@ func (a uint128) Add128(b uint128) uint128 {
         carry = 1
     }
     
-    return uint128{
+    return Uint128{
         H: a.H + b.H + carry,
         L: l,
     }
 }
 
-func (a uint128) Sub128(b uint128) uint128 {
+func (a Uint128) Sub128(b Uint128) Uint128 {
     l := a.L - b.L
     h := a.H - b.H
     
@@ -33,12 +33,12 @@ func (a uint128) Sub128(b uint128) uint128 {
         h -= 1
     }
     
-    return uint128{H: h, L: l}
+    return Uint128{H: h, L: l}
 }
 
 
-func (a uint128) Add32(x int32) uint128 {
-    var b uint128
+func (a Uint128) Add32(x int32) Uint128 {
+    var b Uint128
     b.L = uint64(uint32(x))
     
     if x < 0 {
@@ -48,13 +48,13 @@ func (a uint128) Add32(x int32) uint128 {
     return a.Add128(b)
 }
 
-func (a uint128) Sub32(x int32) uint128 {
+func (a Uint128) Sub32(x int32) Uint128 {
     return a.Add32(-x)
 }
 
 
-func (a uint128) Add64(x int64) uint128 {
-    var b uint128
+func (a Uint128) Add64(x int64) Uint128 {
+    var b Uint128
     b.L = uint64(x)
     
     if x < 0 {
@@ -64,15 +64,15 @@ func (a uint128) Add64(x int64) uint128 {
     return a.Add128(b)
 }
 
-func (a uint128) Sub64(x int64) uint128 {
+func (a Uint128) Sub64(x int64) Uint128 {
     return a.Add64(-x)
 }
 
-func (a uint128) Mul128(b uint128) uint128 {
+func (a Uint128) Mul128(b Uint128) Uint128 {
     
-    mul64 := func (x, y uint64) uint128 {
+    mul64 := func (x, y uint64) Uint128 {
         h, l := bits.Mul64(x, y)
-        return uint128{H: h, L: l}
+        return Uint128{H: h, L: l}
     }
 
     a0, a1 := a.L, a.H
@@ -92,46 +92,46 @@ func (a uint128) Mul128(b uint128) uint128 {
 
     hi := hiHi.L + loHi.H + hiLo.H + a1*b1 + carry
 
-    return uint128{H: hi, L: lo}
+    return Uint128{H: hi, L: lo}
 }
 
-func (a uint128) ShiftLeft(n uint) uint128 {
+func (a Uint128) ShiftLeft(n uint) Uint128 {
     if n >= 128 {
-        return uint128{0, 0}
+        return Uint128{0, 0}
     } else if n == 0 {
         return a
     } else if n < 64 {
-        return uint128{
+        return Uint128{
             H: (a.H << n) | (a.L >> (64 - n)),
             L: a.L << n,
         }
     } else { // 64 <= n < 128
-        return uint128{
+        return Uint128{
             H: a.L << (n - 64),
             L: 0,
         }
     }
 }
 
-func (a uint128) ShiftRight(n uint) uint128 {
+func (a Uint128) ShiftRight(n uint) Uint128 {
     if n >= 128 {
-        return uint128{0, 0}
+        return Uint128{0, 0}
     } else if n == 0 {
         return a
     } else if n < 64 {
-        return uint128{
+        return Uint128{
             H: a.H >> n,
             L: (a.L >> n) | (a.H << (64 - n)),
         }
     } else { // 64 <= n < 128
-        return uint128{
+        return Uint128{
             H: 0,
             L: a.H >> (n - 64),
         }
     }
 }
 
-func (u uint128) String() (s string) {
+func (u Uint128) String() (s string) {
     if u.H > 0 {
         s = fmt.Sprintf("0x%x%016x", u.H, u.L)
     } else {
